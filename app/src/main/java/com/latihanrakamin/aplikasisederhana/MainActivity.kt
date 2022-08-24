14221bb37fd993cc7374e349b53dc795f486f57b
Kelvin <74870436+JRSKelvin@users.noreply.github.com>
2022-08-24 16:28:25 +0700

    Added 5 Fragments And Main Navigation For Navigation Component

--- app/src/main/java/com/latihanrakamin/aplikasisederhana/MainActivity.kt
+++ app/src/main/java/com/latihanrakamin/aplikasisederhana/MainActivity.kt
@@ -2,10 +2,17 @@ package com.latihanrakamin.aplikasisederhana
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import com.latihanrakamin.aplikasisederhana.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar?.hide()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/com/latihanrakamin/aplikasisederhana/fragment/SplashFragment.kt
@@ -0,0 +1,21 @@
+package com.latihanrakamin.aplikasisederhana.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.latihanrakamin.aplikasisederhana.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
